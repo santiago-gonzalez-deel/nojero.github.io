8f865329772fbdd2d0a5cc2ab39b59a9b1e608fc
Santiago Gonzalez <nojero@gmail.com>
2019-01-02 16:46:30 -0300

    Check

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
+import { Words } from 'words';
+
 $("#next").click(() => {
     let turn = parseInt($("#turn").html());
     if (turn == 8) {
         alert("No hay más turnos, el juego terminó");
     } else if (confirm("Seguro que desea avanzar de turno?")) {
+        alert(words[0][0])
         $("#turn").html(turn + 1);
         $(".code").each(function () {
             let val = $(this).data("turn");
